0617ee70920cf6cc3cbaff0066eb92c51dc8f9a9
Blondi Nicholas <blondi.nicholas@osdbsesto>
2022-02-16 09:37:46 +0100

    add server and client main

--- JavaSocket/src/ServerSocket/ServerSocket.java
+++ JavaSocket/src/ServerSocket/ServerSocket.java
@@ -1,31 +1,37 @@
-package ServerSocket;
+package serverSocket;
+
 import java.net.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ *
+ * @author pogliani.mattia
+ */
 public class Server {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          ServerSocket serverSocket = new ServerSocket(5000);
-        Socket clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String greeting = in.readLine();
+        try {
+            java.net.ServerSocket serverSocket = new java.net.ServerSocket(5000);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String greeting = in.readLine();
             if ("hello server".equals(greeting)) {
                 out.println("hello client");
-            }
-            else {
+            } else {
                 out.println("unrecognised greeting");
             }
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    }catch (IOExeption ex){
-        logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-}
-    
 }
